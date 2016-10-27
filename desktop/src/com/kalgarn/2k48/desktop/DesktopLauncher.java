390fe64aba668ad081ed258619adb95620fb8370
Kalgarn <kalgarn@tuta.io>
2016-10-27 14:27:57 +0200

    #1

---
+++ desktop/src/com/kalgarn/2k48/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.kalgarn.2k48.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.kalgarn.2k48.2k48Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new 2k48Game(), config);
+	}
+}
---
+++ html/src/com/kalgarn/2k48/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.kalgarn.2k48.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.kalgarn.2k48.2k48Game;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new 2k48Game();
+        }
+}
